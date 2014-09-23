a6d45cc956c6a6a7c5db10bb6a7d37b889e43c19
Luiz Felipe Cunha <felipe.silvacunha@gmail.com>
2014-09-23 15:48:05 -0300

    Correções da inicialização do módulo

--- Module.php
+++ Module.php
@@ -1,19 +1,14 @@
 <?php
-namespace ABSCore\DataAcess;
+namespace ABSCore\DataAccess;
 
 class Module
 {
-    public function getConfig()
-    {
-        return include __DIR__.'/config/module.config.php';
-    }
-
     public function getAutoloaderConfig()
     {
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\','/',__NAMESPACE__),
                 ),
             ),
         );
