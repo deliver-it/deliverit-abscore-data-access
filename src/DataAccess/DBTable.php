b1af8b576f249979699f037276f6b56ce2762a58
Luiz Felipe Cunha <felipe.silvacunha@gmail.com>
2014-09-24 10:22:10 -0300

    Adição de documentação e refatoração de teste

--- src/DataAccess/DBTable.php
+++ src/DataAccess/DBTable.php
@@ -11,17 +11,71 @@ use Zend\Filter\Word\SeparatorToCamelCase;
 use Exception;
 use ArrayObject;
 
+/**
+ * Interface de acesso à dados de banco de dados.
+ *
+ * Esta implementação abstrai única e exclusivamente uma tabela
+ *
+ * @uses DataAccessInterface
+ * @category ABSCore
+ * @package DataAccess
+ */
 class DBTable implements DataAccessInterface
 {
 
+    /**
+     * Contante que determina o sufixo para o protótipo customizado
+     */
     const PROTOTYPE_SUFFIX = 'Prototype';
 
+    /**
+     * Gerenciador de serviços
+     *
+     * @var Zend\ServiceManager\ServiceLocatorInterface
+     * @access protected
+     */
     protected $serviceLocator;
+
+    /**
+     * Conjunto de chaves Primárias
+     *
+     * @var array
+     * @access protected
+     */
     protected $primaryKey;
+
+    /**
+     * Nome da tabela no banco de dados
+     *
+     * @var string
+     * @access protected
+     */
     protected $tableName;
+
+    /**
+     * Gateway da tabela no banco de dados
+     *
+     * @var Zend\Db\TableGateway\TableGatewayInterface
+     * @access protected
+     */
     protected $tableGateway;
+
+    /**
+     * Adaptador de banco de dados
+     *
+     * @var Zend\Db\Adapter\AdapterInterface
+     * @access protected
+     */
     protected $adapter;
 
+    /**
+     * Contrutor da classe
+     *
+     * @param string $resource Nome do recurso(tabela no banco de dados)
+     * @param mixed $primaryKey Chave(s) primárias
+     * @param Zend\ServiceManager\ServiceLocatorInterface $service Gerenciador de serviços
+     * @access public
+     */
     public function __construct($resource, $primaryKey, ServiceLocatorInterface $service)
     {
         $this->serviceLocator = $service;
@@ -29,6 +83,14 @@ class DBTable implements DataAccessInterface
         $this->setTableName($resource);
     }
 
+    /**
+     * Método para busca de registro único
+     *
+     * @throws Exception Quando o registro não é encontrado
+     * @param mixed $primaryKey Chave(s) primária(s)
+     * @access public
+     * @return mixed Registro encontrado
+     */
     public function find($primaryKey)
     {
         $condition = $this->makeFindCondition($primaryKey);
@@ -52,12 +114,25 @@ class DBTable implements DataAccessInterface
     }
 
 
+    /**
+     * Define o gateway de tabela do banco de dados
+     *
+     * @param TableGatewayInterface $tableGateway
+     * @access public
+     * @return ABSCore\DataAccess\DBTable Próprio objeto para encadeamento
+     */
     public function setTableGateway(TableGatewayInterface $tableGateway)
     {
         $this->tableGateway = $tableGateway;
         return $this;
     }
 
+    /**
+     * Obtenção do gateway de tabela do banco de dados
+     *
+     * @access public
+     * @return Zend\Db\TableGateway\TableGatewayInterface
+     */
     public function getTableGateway()
     {
         if (is_null($this->tableGateway)) {
@@ -66,27 +141,59 @@ class DBTable implements DataAccessInterface
         return $this->tableGateway;
     }
 
+    /**
+     * Obtenção das chaves primárias
+     *
+     * @access public
+     * @return array
+     */
     public function getPrimaryKey()
     {
         return $this->primaryKey;
     }
 
+    /**
+     * Obtenção do nome da tabela
+     *
+     * @access public
+     * @return string
+     */
     public function getTableName()
     {
         return $this->tableName;
     }
 
+    /**
+     * Obtenção do gerenciador de serviços
+     *
+     * @access public
+     * @return Zend\ServiceManager\ServiceLocatorInterface
+     */
     public function getServiceLocator()
     {
         return $this->serviceLocator;
     }
 
+    /**
+     * Define o adaptador de banco de dados
+     *
+     * @param Zend\Db\Adapter\AdapterInterface $adapter
+     * @access public
+     * @return ABSCore\DataAccess\DBTable Próprio objeto para encadeamento
+     */
     public function setAdapter(AdapterInterface $adapter)
     {
         $this->adapter = $adapter;
         return $this;
     }
 
+    /**
+     * Obtenção do adaptador de banco de dados
+     *
+     * @throws Exception quando um adaptador não foi anteriormente definido
+     * @access public
+     * @return Zend\Db\Adapter\AdapterInterface
+     */
     public function getAdapter()
     {
         if (is_null($this->adapter)) {
@@ -95,6 +202,13 @@ class DBTable implements DataAccessInterface
         return $this->adapter;
     }
 
+    /**
+     * Realiza a criação de um gateway de tabela do banco de dados com base no nome da tabela definido em tempo de
+     * construção
+     *
+     * @access protected
+     * @return ABSCore\DataAccess\DBTable Próprio objeto para encadeamento
+     */
     protected function createTableGateway()
     {
         $resultSet = new ResultSet();
@@ -105,43 +219,77 @@ class DBTable implements DataAccessInterface
         return $this;
     }
 
+    /**
+     * Obtenção do protótipo
+     *
+     * @access protected
+     * @return mixed
+     */
     protected function getPrototype()
     {
         $table = $this->getTableName();
+        // montagem do nome do protótipo customizado
         $filter = new SeparatorToCamelCase();
         $prototypeName = $filter->filter($table).self::PROTOTYPE_SUFFIX;
         $serviceLocator = $this->getServiceLocator();
+
+        // existe um serviço para o protótipo?
         if ($serviceLocator->has($prototypeName)) {
             $prototype = $serviceLocator->get($prototypeName);
+        // definição de um protótipo default
         } else {
             $prototype = new ArrayObject();
         }
+
         return $prototype;
     }
 
+    /**
+     * Construção de condições para a busca de um único registro
+     *
+     * @throws Exception para quando uma chave é inválida
+     * @param mixed $primaryKey
+     * @access protected
+     * @return array
+     */
     protected function makeFindCondition($primaryKey) {
+        // verifica as chaves primárias
         $this->verifyPrimaryKey($primaryKey);
+
         if (!is_array($primaryKey)) {
             $primaryKey = array((string)$primaryKey);
         }
 
+        // obtenção das chaves definidas em tempo de contrução
         $keys = $this->getPrimaryKey();
 
         $conditions = array();
+        // construção das condições
         foreach ($primaryKey as $key => $value) {
             if (is_string($key)) {
+                // a chave passada não faz parte do conjunto de chaves da tabela?
                 if (!in_array($key, $keys)) {
                     $message = sprintf('The key "%s" is not a valid primary key (%s)',$key,implode(',',$keys));
                     throw new Exception($message);
                 }
+
                 $conditions[$key] = $value;
             } else {
                 $conditions[$keys[$key]] = $value;
             }
         }
+
         return $conditions;
     }
 
+    /**
+     * Realiza a verificação de um conjunto de chaves com o conjunto de chaves da tabela
+     *
+     * @throws Exception quando a quantidade de chaves passadas é diferente da quantidade de chaves da tabela
+     * @param mixed $primaryKey
+     * @access protected
+     * @return ABSCore\DataAccess\DBTable Próprio objeto para encadeamento
+     */
     protected function verifyPrimaryKey($primaryKey)
     {
         $primaryCount = count($this->getPrimaryKey());
@@ -150,6 +298,7 @@ class DBTable implements DataAccessInterface
         } else {
             $passed = count($primaryKey);
         }
+
         if ($passed != $primaryCount) {
             $message = sprintf('%d keys are expected but %d was passed', $primaryCount, $passed);
             throw new Exception($message);
@@ -158,6 +307,14 @@ class DBTable implements DataAccessInterface
         return $this;
     }
 
+    /**
+     * Define o conjunto de chaves primárias
+     *
+     * @throws Exception quando o conjunto de chaves é vazio
+     * @param mixed $primaryKey
+     * @access protected
+     * @return ABSCore\DataAccess\DBTable Próprio objeto para encadeamento
+     */
     protected function setPrimaryKey($primaryKey)
     {
         if (empty($primaryKey)) {
@@ -171,6 +328,14 @@ class DBTable implements DataAccessInterface
         return $this;
     }
 
+    /**
+     * Define o nome da tabela do banco de dados
+     *
+     * @throws Exception quando o nome da tabela é vazio
+     * @param string $name Nome da tabela
+     * @access protected
+     * @return ABSCore\DataAccess\DBTable Próprio objeto para encadeamento
+     */
     protected function setTableName($name)
     {
         $name = (string)$name;
@@ -178,5 +343,7 @@ class DBTable implements DataAccessInterface
             throw new Exception('The table name cannot be blank!');
         }
         $this->tableName = $name;
+
+        return $this;
     }
 }
--- test/DataAccessTest/DBTableTest.php
+++ test/DataAccessTest/DBTableTest.php
@@ -13,12 +13,24 @@ use ABSCore\DataAccess\DBTable;
 class DBTableTest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Teste para quando o nome do recurso é inválido
+     *
+     * @access public
+     * @return null
+     */
     public function testInvalidResourceName()
     {
         $this->setExpectedException('Exception','The table name cannot be blank!');
         $dbTable = new DBTable('',array('id'), $this->getServiceManager());
     }
 
+    /**
+     * Teste para quando as chaves primárias são inválidas
+     *
+     * @access public
+     * @return null
+     */
     public function testInvalidPrimaryKey()
     {
         $this->setExpectedException('Exception','At last one primary key must be passed');
@@ -26,6 +38,12 @@ class DBTableTest extends PHPUnit_Framework_TestCase
     }
 
 
+    /**
+     * Teste para quando não é definido um adaptador
+     *
+     * @access public
+     * @return null
+     */
     public function testNonSetedAdapter()
     {
         $this->setExpectedException('Exception','An adapter is required');
@@ -33,6 +51,12 @@ class DBTableTest extends PHPUnit_Framework_TestCase
         $dbTable->getAdapter();
     }
 
+    /**
+     * Teste de busca sem resultado
+     *
+     * @access public
+     * @return null
+     */
     public function testFindWithoutResult()
     {
         $this->setExpectedException('Exception','registry not found');
@@ -47,6 +71,12 @@ class DBTableTest extends PHPUnit_Framework_TestCase
         $dbTable->find('1');
     }
 
+    /**
+     * Teste de chaves primárias inválidas
+     *
+     * @access public
+     * @return null
+     */
     public function testFindWithInvalidPrimaryKeys()
     {
         $this->setExpectedException('Exception', '2 keys are expected but 1 was passed');
@@ -56,6 +86,12 @@ class DBTableTest extends PHPUnit_Framework_TestCase
         $this->assertNotNull($dbTable->find('1'));
     }
 
+    /**
+     * Teste de busca passando uma chave inválida
+     *
+     * @access public
+     * @return null
+     */
     public function testFindWithNonPrimaryKeys()
     {
         $this->setExpectedException('Exception', 'The key "teste" is not a valid primary key (id)');
@@ -65,17 +101,25 @@ class DBTableTest extends PHPUnit_Framework_TestCase
         $this->assertNotNull($dbTable->find(array('teste' => 1)));
     }
 
+    /**
+     * Teste de busca com sucesso
+     *
+     * @access public
+     * @return null
+     */
     public function testFind()
     {
         $dbTable = new DBTable('teste','id', $this->getServiceManager());
-        $resultSet = new ResultSet();
-        $resultSet->initialize(array(array('id' => 1)));
-        $tableMock = $this->getTableGatewayMock();
-        $tableMock->expects($this->once())->method('select')->will($this->returnValue($resultSet));
-        $dbTable->setTableGateway($tableMock);
+        $dbTable->setAdapter($this->getAdapterMock());
         $this->assertNotNull($dbTable->find('1'));
     }
 
+    /**
+     * Teste de busca com múltiplas chaves
+     *
+     * @access public
+     * @return null
+     */
     public function testFindWithMultipleKeys()
     {
         $dbTable = new DBTable('teste',array('id','id2'), $this->getServiceManager());
@@ -87,7 +131,33 @@ class DBTableTest extends PHPUnit_Framework_TestCase
         $this->assertNotNull($dbTable->find(array('id' => 1, 'id2' => 2)));
     }
 
+    /**
+     * Teste para quando um protótipo customizado é definido
+     *
+     * @access public
+     * @return null
+     */
     public function testCustomPrototype()
+    {
+        $result = $this->getMockBuilder('ArrayObject')->getMock();
+        $result->expects($this->once())->method('exchangeArray');
+
+        $service = $this->getServiceManager();
+        $service->setService('TestePrototype',$result);
+
+        $dbTable = new DBTable('teste','id', $service);
+        $dbTable->setAdapter($this->getAdapterMock());
+
+        $dbTable->find('1');
+    }
+
+    /**
+     * Método auxiliar para obter um adaptador Mocado
+     *
+     * @access protected
+     * @return null
+     */
+    protected function getAdapterMock()
     {
         $adapter = $this->getMockBuilder('Zend\Db\Adapter\Adapter')
             ->disableOriginalConstructor()
@@ -111,24 +181,27 @@ class DBTableTest extends PHPUnit_Framework_TestCase
         $adapter->method('getPlatform')->will($this->returnValue($platform));
         $adapter->method('getDriver')->will($this->returnValue($driver));
 
-        $result = $this->getMockBuilder('ArrayObject')->getMock();
-        $result->expects($this->once())->method('exchangeArray');
-
-        $service = $this->getServiceManager();
-        $service->setService('TestePrototype',$result);
-
-        $dbTable = new DBTable('teste','id', $service);
-        $dbTable->setAdapter($adapter);
-
-        $dbTable->find('1');
+        return $adapter;
     }
 
+    /**
+     * Obtenção de gerenciador de serviços
+     *
+     * @access protected
+     * @return null
+     */
     protected function getServiceManager()
     {
         return new ServiceManager(new ServiceConfig());
     }
 
 
+    /**
+     * Obtenção do mock de TableGateway
+     *
+     * @access protected
+     * @return null
+     */
     protected function getTableGatewayMock()
     {
         return $this->getMockBuilder('Zend\Db\TableGateway\TableGateway')
