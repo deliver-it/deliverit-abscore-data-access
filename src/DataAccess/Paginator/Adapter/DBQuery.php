4db0af7f92cb66e968300088688c430174abb388
Bruno Paixao <bruno@absoluta.net>
2015-06-30 17:28:31 -0300

    Fixed problem when use quantifier combined with limit and offset in oracle

--- src/DataAccess/Paginator/Adapter/DBQuery.php
+++ src/DataAccess/Paginator/Adapter/DBQuery.php
@@ -47,16 +47,31 @@ class DBQuery implements AdapterInterface
 
         // get IDS
         $select
-            ->limit($itemCountPerPage)
-            ->offset($offset)
             ->columns($ids)
             ->quantifier(Sql\Select::QUANTIFIER_DISTINCT);
-        $result = $table->getTableGateway()->selectWith($select);
+
+        $joinConditions = [];
+        foreach ($ids as $id) {
+            $joinConditions[] = "t.$id = s.$id";
+        }
+
+
+        $select2 = new Sql\Select();
+        $select2
+            ->columns($ids)
+            ->from(['t' => $table->getTableName()])
+            ->join(['s' => $select], implode(' AND ', $joinConditions), [])
+            ->limit($itemCountPerPage)
+            ->offset($offset);
+        $adapter = $table->getTableGateway()->getAdapter();
+        $sql = new Sql\Sql($adapter);
+        $statement = $sql->prepareStatementForSqlObject($select2);
+        $result = $statement->execute();
 
         // make conditions
         $tableName = $table->getTableName();
         $conditions = array();
-        foreach ($result->toArray() as $row) {
+        foreach ($result as $row) {
             foreach ($row as $col => $value) {
                 $conditions[$tableName.'.'.$col][] = $value;
             }
@@ -64,7 +79,7 @@ class DBQuery implements AdapterInterface
         // fetch curret page
         $query->getSelect()->where($conditions);
         $result = $query->fetch();
-
+        $result = new \ArrayIterator($result->toArray());
         return $result;
     }
 
@@ -77,6 +92,14 @@ class DBQuery implements AdapterInterface
     public function count()
     {
         $select = clone $this->query->getSelect();
+        $query = $this->query;
+        $table = $query->getFromTable();
+        $joins = $select->getRawState($select::JOINS);
+        $select->reset($select::JOINS);
+        $ids = $table->getPrimaryKey();
+        foreach ($joins as $join) {
+            $select->join($join['name'], $join['on'], array(), $join['type']);
+        }
         $countExpression = new Sql\Expression('COUNT(DISTINCT '.implode(', ', $this->getPrimaryKeys()).')');
         $select->columns(array('c' => $countExpression));
         $table = $this->query->getFromTable();
